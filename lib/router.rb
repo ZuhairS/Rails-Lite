b87623c2494e858e431271872c276ced8bd6802d
Zuhair A. Shaikh <zuhair.a.shaikh@outlook.com>
2017-06-27 17:58:05 -0700

    Complete more methods in Router class

--- lib/router.rb
+++ lib/router.rb
@@ -17,6 +17,7 @@ class Route
   # instantiate controller and call controller action
   def run(req, res)
     # @params = @pattern.match(req.path)
+    @controller_class.new(req, res, {}).invoke_action(@action_name)
     # eval("#{@controller_class}.new.#{@action_name}")
   end
 end
@@ -25,12 +26,12 @@ class Router
   attr_reader :routes
 
   def initialize
-    @router = []
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
-    @router << Route.new(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   # evaluate the proc in the context of the instance
@@ -41,14 +42,23 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
-    define_method(http_method) { add_route(pattern, method, controller_class, action_name) }
+    define_method(http_method) do |pattern, method, controller_class, action_name|
+      add_route(pattern, method, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.find { |route| route.matches?(req) }
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
+    matched_route = match(req)
+    if matched_route
+      matched_route.run(req, res)
+    else
+      res.status = [404, "Link not found. Please check your address."]
+    end
   end
 end
